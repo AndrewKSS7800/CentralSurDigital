4c0203f3070b09e7d969f426c2db1a470a402a0f
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-03-22 16:08:33 -0500

    Arquictura de React - Colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import About from './pages/about';
