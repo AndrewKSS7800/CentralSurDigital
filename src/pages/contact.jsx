1749646fd16d4087fd753290906e90af66969f91
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-03-27 16:52:19 -0500

    Arquitectura de React - Pestaña Contact Responsive

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,19 +1,67 @@
+import IMGCONTACT from "../assets/imgs/img-contact.jpg"
 const Contact = () => {
     return (
-        <section>
+        <section className="contact">
             <div className="cpar">
                 <div className="par">
-                    <span></span>
-                    <p></p>
-                    <div className="clinks"></div>
+                    <span>En Central Sur Digital, ¡tu voz es fundamental!</span>
+                    <p>
+                        Sabemos que la comunicación es clave, y queremos estar siempre cerca de nuestra comunidad.
+                        <br />
+                        Si tienes alguna pregunta, una sugerencia que pueda mejorar nuestro trabajo, o simplemente deseas conocernos mejor, estamos aquí para escucharte. 
+                        
+                        Nuestro equipo está siempre disponible para atenderte y brindarte la mejor experiencia posible. No dudes en escribirnos, compartir tus ideas o expresar tus inquietudes. ¡Tu opinión nos ayuda a crecer y mejorar cada día!
+                        <br />
+                        Contáctanos y se parte de nuestra comunidad informativa.
+                    </p>
+                    <div className="clinks">
+                        <ul>
+                            <li><a href="https://www.youtube.com/@CentralSurDigital/videos"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M508.64 148.79c0-45-33.1-81.2-74-81.2C379.24 65 322.74 64 265 64h-18c-57.6 0-114.2 1-169.6 3.6C36.6 67.6 3.5 104 3.5 149 1 184.59-.06 220.19 0 255.79q-.15 53.4 3.4 106.9c0 45 33.1 81.5 73.9 81.5 58.2 2.7 117.9 3.9 178.6 3.8q91.2.3 178.6-3.8c40.9 0 74-36.5 74-81.5 2.4-35.7 3.5-71.3 3.4-107q.34-53.4-3.26-106.9zM207 353.89v-196.5l145 98.2z"/></svg></a></li>
+                            <li><a href="https://www.tiktok.com/@centralsurdigital"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M412.19 118.66a109.27 109.27 0 01-9.45-5.5 132.87 132.87 0 01-24.27-20.62c-18.1-20.71-24.86-41.72-27.35-56.43h.1C349.14 23.9 350 16 350.13 16h-82.44v318.78c0 4.28 0 8.51-.18 12.69 0 .52-.05 1-.08 1.56 0 .23 0 .47-.05.71v.18a70 70 0 01-35.22 55.56 68.8 68.8 0 01-34.11 9c-38.41 0-69.54-31.32-69.54-70s31.13-70 69.54-70a68.9 68.9 0 0121.41 3.39l.1-83.94a153.14 153.14 0 00-118 34.52 161.79 161.79 0 00-35.3 43.53c-3.48 6-16.61 30.11-18.2 69.24-1 22.21 5.67 45.22 8.85 54.73v.2c2 5.6 9.75 24.71 22.38 40.82A167.53 167.53 0 00115 470.66v-.2l.2.2c39.91 27.12 84.16 25.34 84.16 25.34 7.66-.31 33.32 0 62.46-13.81 32.32-15.31 50.72-38.12 50.72-38.12a158.46 158.46 0 0027.64-45.93c7.46-19.61 9.95-43.13 9.95-52.53V176.49c1 .6 14.32 9.41 14.32 9.41s19.19 12.3 49.13 20.31c21.48 5.7 50.42 6.9 50.42 6.9v-81.84c-10.14 1.1-30.73-2.1-51.81-12.61z"/></svg></a></li>
+                            <li><a href="https://x.com/centralsur_d"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg></a></li>
+                            <li><a href="https://www.instagram.com/centralsurdigital/"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M349.33 69.33a93.62 93.62 0 0193.34 93.34v186.66a93.62 93.62 0 01-93.34 93.34H162.67a93.62 93.62 0 01-93.34-93.34V162.67a93.62 93.62 0 0193.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z"/><path d="M377.33 162.67a28 28 0 1128-28 27.94 27.94 0 01-28 28zM256 181.33A74.67 74.67 0 11181.33 256 74.75 74.75 0 01256 181.33m0-37.33a112 112 0 10112 112 112 112 0 00-112-112z"/></svg></a></li>
+                            <li><a href="https://web.facebook.com/profile.php?id=61566849996187"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189 221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15 4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31z" fill-rule="evenodd"/></svg></a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="cimg">
-                    <img src="" alt="" />
+                    <img src={IMGCONTACT} alt="" />
+                </div>
+            </div>
+            <div className="ccon">
+                <div className="ccont">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="auto" viewBox="0 -960 960 960" width="auto" fill="#e3e3e3" className="c-icon"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg></i>
+                    <span>Villa el salvador</span>
+                </div>
+                <div className="ccont">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="auto" viewBox="0 -960 960 960" width="auto" fill="#e3e3e3" className="c-icon"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg></i>
+                    <span>+51 978665437</span>
+                </div>
+                <div className="ccont">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="auto " viewBox="0 -960 960 960" width="auto" fill="#e3e3e3" className="c-icon"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg></i>
+                    <span>CentralSurDigital@gmail.com</span>
                 </div>
             </div>
             <div className="cform">
-                
-                <form action=""></form>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15610.260524004667!2d-77.06525478331298!3d-12.004563745032538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105ce568c042771%3A0x6072f46c2b26e80!2sSENATI%20Sede%20Central!5e0!3m2!1ses!2spe!4v1743096465632!5m2!1ses!2spe" width="100%" height="auto" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <form action="">
+                    <span className="ftitle">Contáctanos</span>
+                    <label htmlFor="email">
+                        <input required type="text" id="email" name="email" />
+                        <span>Gmail</span>
+                    </label>
+                    <label htmlFor="name">
+                        <input required type="text" id="name" name="name" />
+                        <span>Nombre</span>
+                    </label>
+                    <label htmlFor="message">
+                        <input required type="text" id="message" name="message" />
+                        <span>Mensaje</span>
+                    </label>
+                    <button>
+                        <span>Enviar</span>
+                    </button>
+                </form>
             </div>
         </section>
     );
