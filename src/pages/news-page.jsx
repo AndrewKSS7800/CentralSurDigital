4f7bad721ae3acd4772dcde441815c7eb15c03c8
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-04-24 14:23:27 -0500

    Botones para compartir noticias 06

--- src/pages/news-page.jsx
+++ src/pages/news-page.jsx
@@ -27,7 +27,7 @@ const Newspage = () =>{
                 <title>{noticia.title}</title>
                 <meta property="og:title" content={noticia.title} />
                 <meta property="og:description" content={noticia.content.substring(0, 150)} />
-                <meta property="og:image" content={`/imgs/${noticia.img}.png`} />
+                <meta property="og:image" content={`${process.env.PUBLIC_URL}/imgs/${noticia.img}.png`} />
                 <meta property="og:url" content={`https://centralsurdigital.netlify.app/noticias/${noticia.id}`} />
                 <meta property="og:type" content="article" />
                 
@@ -35,7 +35,7 @@ const Newspage = () =>{
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content={noticia.title} />
                 <meta name="twitter:description" content={noticia.content.substring(0, 150)} />
-                <meta name="twitter:image" content={`/imgs/${noticia.img}.png`} />
+                <meta name="twitter:image" content={`${process.env.PUBLIC_URL}/imgs/${noticia.img}.png`} />
             </Helmet>
             <div className="npback">
                 <div onClick={() => navigate(-1)}><svg xmlns="http://www.w3.org/2000/svg" className="npico" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m480-320 56-56-64-64h168v-80H472l64-64-56-56-160 160 160 160Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></div> <span></span> <span> {noticia.category}</span> <span>/</span> <span> {noticia.title}</span>
