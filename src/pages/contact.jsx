e1cfe65d9ba9de2b8aab8c81cae324bd892649ef
Luis Ángel Acevedo Córdova <1462427@senati.pe>
2025-05-06 13:04:54 -0500

    Enviar correo

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,34 @@
-import IMGCONTACT from "../assets/imgs/img-contact.jpg"
+import IMGCONTACT from "../assets/imgs/img-contact.jpg";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm(
+            "service_zy5cxfp",    
+            "template_mdhv25d",     
+            form.current,
+            "rwNkNHXEs8wtNWtMf"       
+        )
+        .then(
+            (result) => {
+            console.log("Correo enviado con éxito:", result.text);
+            alert("✅ Mensaje enviado correctamente");
+            form.current.reset(); // limpia el formulario
+            },
+            (error) => {
+            console.log("Error:", error.text);
+            alert("❌ Error al enviar mensaje");
+            }
+        );
+    };
+
+
     return (
         <section className="contact">
             <div className="cpar">
@@ -29,22 +58,30 @@ const Contact = () => {
                 </div>
             </div>
             <div className="ccon">
-                <a href="https://maps.app.goo.gl/Zpfb1P1DLVBSUAYf7" target="_blank" className="ccont">
+                <a href="https://maps.app.goo.gl/mZ4etsWpP3V2eytB9" target="_blank" className="ccont">
                     <i><svg xmlns="http://www.w3.org/2000/svg" height="auto" viewBox="0 -960 960 960" width="auto" fill="#e3e3e3" className="c-icon"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg></i>
-                    <span>Villa el salvador</span>
+                    <span>Villa María del Triunfo</span>
                 </a>
                 <a href="https://wa.me/51995855383" target="_blank" className="ccont">
                     <i><svg xmlns="http://www.w3.org/2000/svg" height="auto" viewBox="0 -960 960 960" width="auto" fill="#e3e3e3" className="c-icon"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg></i>
-                    <span>+51 978665437</span>
+                    <span>+51 995855383</span>
                 </a>
                 <a href="mailto:centralsurdigitalnoticias@gmail.com" target="_blank" className="ccont">
                     <i><svg xmlns="http://www.w3.org/2000/svg" height="auto " viewBox="0 -960 960 960" width="auto" fill="#e3e3e3" className="c-icon"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg></i>
-                    <span>CentralSurDigital@gmail.com</span>
+                    <span>CentralSurDigitalNoticias@gmail.com</span>
                 </a>
             </div>
             <div className="cform">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15610.260524004667!2d-77.06525478331298!3d-12.004563745032538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105ce568c042771%3A0x6072f46c2b26e80!2sSENATI%20Sede%20Central!5e0!3m2!1ses!2spe!4v1743096465632!5m2!1ses!2spe" width="100%" height="auto" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <form action="">
+                <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.5358621521405!2d-76.9518099413721!3d-12.143876846754269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b8917cb616cb%3A0x354de2992530dcfa!2sMonzon%20289%2C%20Villa%20Mar%C3%ADa%20del%20Triunfo%2015812!5e0!3m2!1ses!2spe!4v1746550975953!5m2!1ses!2spe"
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+                <form ref={form} onSubmit={sendEmail}>
                     <span className="ftitle">Contáctanos</span>
                     <label htmlFor="email">
                         <input required type="text" id="email" name="email" />
